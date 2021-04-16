1fe315207085feb4caf3a9a510657fab0bceca3e
lokman <lokman@mit.edu>
2021-04-15 21:02:47 -0400

    implement and test WebServer.ConvertUnits

--- src/citrine_challenge/WebServer.java
+++ src/citrine_challenge/WebServer.java
@@ -1,6 +1,27 @@
 package citrine_challenge;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+import org.json.simple.JSONObject;
 
 public class WebServer {
+    /**
+     * 
+     * 
+     * @param input Properly formatted unit string, as defined in prompt.pdf
+     * @return obj JSON object, as specified in prompt.pdf
+     */
+    public JSONObject ConvertUnits(String input) {
+        Expression expr = Expression.parse(input);
+        String stringSI = expr.toStringSI();
+        Double factor = expr.getFactor(); 
+        BigDecimal bd = new BigDecimal(factor); // 14 sf
+        BigDecimal factor14sf = bd.round(new MathContext(14));
+        JSONObject obj = new JSONObject();
+        obj.put("unit_name", stringSI);
+        obj.put("multiplication_factor", factor14sf);
+        return obj;
+    }
 
 }
--- test/citrine_challenge/WebServerTest.java
+++ test/citrine_challenge/WebServerTest.java
@@ -2,13 +2,22 @@ package citrine_challenge;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.io.StringWriter;
 
+import org.junit.jupiter.api.Test;
+import org.json.simple.JSONObject;
 
 class WebServerTest {
 
-    @Test void test() {
-        fail("Not yet implemented");
+    @Test void test() throws IOException {
+        WebServer server = new WebServer();
+        JSONObject obj = server.ConvertUnits("(degree/minute)");
+        StringWriter out = new StringWriter();
+        obj.writeJSONString(out);
+        
+        String jsonText = out.toString();
+        System.out.print(jsonText);
     }
 
 }
